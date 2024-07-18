b27eb418f40f11df77d18249c91bad96927cbc71
Sergej Görzen <sergej-goerzen@web.de>
2024-07-18 15:41:42 +0200

    .

---
+++ Editor/CustomMenu.cs
@@ -0,0 +1,14 @@
+using UnityEditor;
+
+namespace OmiLAXR.Editor
+{
+    internal static class CustomMenu
+    {
+        [MenuItem("OmiLAXR / Pipeline / Create custom job")]
+        private static void CreateCustomJobScript() {}
+        [MenuItem("OmiLAXR / Pipeline / Create custom stage")]
+        private static void CreateCustomStageScript() {}
+        [MenuItem("OmiLAXR / Pipeline / Create custom pipeline")]
+        private static void CreateCustomStageScript() {}
+    }
+}
