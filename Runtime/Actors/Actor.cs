997ae88f6a8727e56afcc59951fd96a73c76209b
Sergej Görzen <sergej-goerzen@web.de>
2024-08-15 18:48:49 +0200

    refactoring actor

--- Runtime/Actors/Actor.cs
+++ Runtime/Actors/Actor.cs
@@ -5,7 +5,7 @@ namespace OmiLAXR
     [AddComponentMenu("OmiLAXR / 0) Pipelines / Actor")]
     public class Actor : MonoBehaviour
     {
-        public string name = "Anonymous";
-        public string email = "anonymous@omilaxr.dev";
+        public string actorName = "Anonymous";
+        public string actorEmail = "anonymous@omilaxr.dev";
     }
 }
