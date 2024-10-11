3a45856151ce367a1a58332521015ac26d753e75
Sergej Görzen <sergej-goerzen@web.de>
2024-10-11 18:09:49 +0200

    added statement printer

---
+++ Runtime/Endpoints/StatementPrinter.cs
@@ -0,0 +1,15 @@
+using OmiLAXR.Composers;
+using UnityEngine;
+
+namespace OmiLAXR.Endpoints
+{
+    [AddComponentMenu("OmiLAXR / 6) Endpoints / Statement Printer")]
+    public class StatementPrinter : Endpoint
+    {
+        protected override TransferCode HandleSending(IStatement statement)
+        {
+            DebugLog.OmiLAXR.Print("Sent statement: " + statement.ToString());
+            return TransferCode.Success;
+        }
+    }
+}
